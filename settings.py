d7127eaa19dbea54ccd93446dd87bea508b8525e
wavestats <seadas-user@localhost>
2020-05-05 17:38:43 -0400

    minor refactoring

--- settings.py
+++ settings.py
@@ -4,8 +4,8 @@ with open("keybinds.txt", "r") as f, open("env.txt", "r") as e:
     keybind_file_lines = [line.rstrip("\n") for line in f.readlines()]
     symbol_file_lines = [line.rstrip("\n") for line in e.readlines()]
 
-symbols = {}
-symbols_list = []
+symbols = {}  # {"Collision": "X", "Empty Space": " ", ...}
+symbols_list = []  # ["X", " ", "#", "S", ...]
 for i in range(0, len(symbol_file_lines), 3):
     symbol_name = symbol_file_lines[i].lower()
     value = symbol_file_lines[i+1]
@@ -14,7 +14,9 @@ for i in range(0, len(symbol_file_lines), 3):
 
 # So to add symbols, it would appear that we would need to add the double to the end of the list, and then add the symbol to the end of env file
 symbol_doubles_list = [1.0, 50.0, 4.0, 5.0, 6.0, 10.0, 9.0, 12.0, 211.0, 210.0, 22.0]
-obj_symbols_list = symbols_list[3:]
+objs_start = 6
+obj_symbols_list = symbols_list[objs_start:]
+obj_names = [symbol_name for symbol_name in symbols.keys()][objs_start:]
 symbol_doubles = {symbol:double for symbol, double in zip(obj_symbols_list, symbol_doubles_list)}
 double_symbols = {double:symbol for symbol, double in symbol_doubles.items()}
 
@@ -22,8 +24,7 @@ keybinds = {}
 movement_keys = []
 placement_keys = []
 placement_key_symbols = {}  # keyboardkey:icon
-# Well, if I figure this out... names_of_obj_corresponding_to_key = {}
-symbols_list_index = 3  # we only want placement symbols which is why we start at 3
+symbols_list_index = objs_start  # we only want placement symbols which is why we start at objs_start
 for loop_count, i in enumerate(range(0, len(keybind_file_lines), 3)):
     key_name = keybind_file_lines[i].lower()
     values = keybind_file_lines[i+1].split(",")
@@ -39,4 +40,4 @@ for loop_count, i in enumerate(range(0, len(keybind_file_lines), 3)):
 
 
 if __name__ == "__main__":
-    print(str(obj_symbols_list))
+    print(str(symbols))
